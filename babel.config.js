446df84b3c1466d6f763d771de908867a7d08e91
Marcelo Ragnelli Frigerio <mrfrigerio@yahoo.com.br>
2022-12-19 08:23:08 -0300

    Initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: [
+      ["babel-preset-expo"],
+      ["@babel/preset-env", { targets: { node: "current" } }],
+      [
+        "module:metro-react-native-babel-preset",
+        { targets: { node: "current" } },
+      ],
+    ],
+    plugins: [
+      ["react-native-reanimated/plugin"],
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ],
   };
 };
